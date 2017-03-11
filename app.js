4e375e95d1c3ae73f21a7f5fe89dcd60dc7a3555
natashabibbs <nbibbs2411@myhta.org>
2017-03-10 14:28:34 -1000

    I finished all the things. I feel so special

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+console.log("Hello World");
 function fortuneTeller(){
-
+    var numOfKids = 3; // number 
+    var partnerName = "a good meme lover"; //string
+    var geographicLocation = "in the middle of da desert"; //string
+    var jobTitle = "MeMe Dealer";//string
+    alert ("You will be a " + jobTitle + " in " + geographicLocation + " , and married to " + partnerName + " with "+ numOfKids + " kids.");
 }
 
+fortuneTeller();
+
 function ageCalculator(){
+	var birthYear = 2003;//number
+	var myFutureYear = 2025;//number
+	var wowImSoold = 2025 - 2003;//number
+	var dangimSuperOld = 21;//number
+	alert ("I will be either "  +  wowImSoold +  " or " + dangimSuperOld +" in "+ myFutureYear);
+}
+
+ageCalculator();
 
 }
